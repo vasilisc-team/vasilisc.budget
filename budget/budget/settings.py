a3cdd83a12549700c3e19f5787f07f2e62c45156
unknown <seryoga_mayamsin@mail.ru>
2021-09-04 03:46:57 +0300

    plug4

--- budget/budget/settings.py
+++ budget/budget/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'sslserver'
+    'sslserver',
+    'django_plotly_dash.apps.DjangoPlotlyDashConfig',
 ]
 
 MIDDLEWARE = [
