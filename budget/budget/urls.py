9ec5ee05582851c052d521a7c7003c9f43233b8b
unknown <seryoga_mayamsin@mail.ru>
2021-09-04 04:16:27 +0300

    plug4

--- budget/budget/urls.py
+++ budget/budget/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path
 from dash_app import example
 from home.views import home_view
-import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
