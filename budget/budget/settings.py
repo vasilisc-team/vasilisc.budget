540513c52e08e245c2f840855a0db22490b9b4ff
Andrey Tolstykh <xausssr@gmail.com>
2021-09-04 09:49:11 +0300

    hotfix

--- budget/budget/settings.py
+++ budget/budget/settings.py
@@ -128,6 +128,6 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = ( os.path.join(BASE_DIR, "static"), )
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, "static"), ]
 
-STATIC_ROOT = ( os.path.join(BASE_DIR, "static"), )
+STATIC_ROOT = [ os.path.join(BASE_DIR, "static"), ]
