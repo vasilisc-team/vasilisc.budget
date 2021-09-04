a5931b1ac42e0ca062726400d49e78e53e15538e
unknown <seryoga_mayamsin@mail.ru>
2021-09-05 00:23:55 +0300

    +forecast2

--- budget/home/views.py
+++ budget/home/views.py
@@ -1,7 +1,11 @@
 import json
 
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseBadRequest, JsonResponse
 from django_plotly_dash.templatetags import plotly_dash
+import json
+import plotly.graph_objs as go
+import plotly
 
 
 def home_view(request):
@@ -29,8 +33,13 @@ def research_view(request):
 
 
 def forecast_view(request):
+    with open("fig.json") as read_file:
+        data = json.load(read_file)
+        data = json.loads(data)
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=[1, 2, 3], y=[1, 2, 3]))
+    plot_div = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
     template = 'home/forecast.html'
-
-    return render(request, template)
+    return render(request, template,  {'dinamic': plot_div, 'nalognapr': plot_div, 'prognoz': plot_div, 'ndfl': plot_div})
 
 
