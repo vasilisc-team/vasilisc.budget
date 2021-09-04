50f55086781513c9f393a1897604bd46fe2700dc
unknown <seryoga_mayamsin@mail.ru>
2021-09-04 18:13:23 +0300

    +research

--- budget/budget/urls.py
+++ budget/budget/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from dash_app import example
-from home.views import home_view, team_view, content_view
+from home.views import home_view, team_view, content_view, research_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +24,5 @@ urlpatterns = [
     path('home', home_view),
     path('team', team_view),
     path('content', content_view),
+    path('research', research_view),
 ]
--- budget/home/views.py
+++ budget/home/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django_plotly_dash.templatetags import plotly_dash
+import json
 
 
 def home_view(request):
     template = 'home/index.html'
     greeting = '🥥    КЛАСИВЫЙ ДИЗИГН ОТ ДИЗИГНЕРА КОКОСА   🥥'
+
+    with open("fig.json", "r") as read_file:
+        data = json.load(read_file)
+
     context = {
         'greeting': greeting
     }
@@ -24,6 +29,15 @@ def team_view(request):
 
 def content_view(request):
     template = 'home/content.html'
+    greeting = '🥥    КЛАСИВЫЙ ДИЗИГН ОТ ДИЗИГНЕРА КОКОСА   🥥'
+    context = {
+        'greeting': greeting
+    }
+    return render(request, template, context)
+
+
+def research_view(request):
+    template = 'home/research.html'
     greeting = '🥥    КЛАСИВЫЙ ДИЗИГН ОТ ДИЗИГНЕРА КОКОСА   🥥'
     context = {
         'greeting': greeting
